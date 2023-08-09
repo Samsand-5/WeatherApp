fc67d93b2c70597dc0680a3a8a6e5434ac1bde87
coder <soumyodipsutra@gmail.com>
2023-08-09 23:43:11 +0530

    design

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.location.LocationManagerCompat.getCurrentLocation
+import androidx.core.location.LocationManagerCompat.isLocationEnabled
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+
+
+        getCurrentLocation()
+    }
+
+    private fun checkPermissions(): Boolean {
+
+    }
+
+    private fun  getCurrentLocation(){
+
+        if(checkPermissions()){
+            if(isLocationEnabled()){
+                if(ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
+                ) != packageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission())
+            }
+        }
     }
 }
